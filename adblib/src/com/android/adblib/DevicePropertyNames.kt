f5c7d7a1ab491f8d70646c8b9e4cb7edfc9ab3c4
James Rose <rosej@google.com>
2023-08-25 16:52:40 -0700

    Add DeviceInfo proto to DeviceProperties for metrics
    The DeviceProperties now contains a DeviceInfo proto that can be
    directly used in AndroidStudioEvent. It is populated based on the
    existing DeviceProperties fields, as well as the serial number of the
    device and other system properties.
    
    Bug: 290117879
    Test: added / updated
    Change-Id: I0af94401fc5134a5f633aef9d97c9f39fa2155ef

--- adblib/src/com/android/adblib/DevicePropertyNames.kt
+++ adblib/src/com/android/adblib/DevicePropertyNames.kt
@@ -15,19 +15,22 @@
  */
 package com.android.adblib
 
-/** Names of commonly used device properties.  */
+/** Names of commonly used device properties. */
 object DevicePropertyNames {
     const val RO_BOOT_QEMU_AVD_NAME = "ro.boot.qemu.avd_name"
     const val RO_BUILD_CHARACTERISTICS = "ro.build.characteristics"
+    const val RO_BUILD_TAGS = "ro.build.tags"
+    const val RO_BUILD_TYPE = "ro.build.type"
     const val RO_BUILD_VERSION_CODENAME = "ro.build.version.codename"
     const val RO_BUILD_VERSION_RELEASE = "ro.build.version.release"
     const val RO_BUILD_VERSION_SDK = "ro.build.version.sdk"
+    const val RO_KERNEL_QEMU = "ro.kernel.qemu"
     const val RO_KERNEL_QEMU_AVD_NAME = "ro.kernel.qemu.avd_name" // Older version of RO_BOOT_QEMU_AVD_NAME
     const val RO_MANUFACTURER = "ro.manufacturer" // Older version of RO_PRODUCT_MANUFACTURER
     const val RO_MODEL = "ro.model" // Older version of RO_PRODUCT_MODEL
     const val RO_PRODUCT_CPU_ABI = "ro.product.cpu.abi"
     const val RO_PRODUCT_MANUFACTURER = "ro.product.manufacturer"
     const val RO_PRODUCT_MODEL = "ro.product.model"
-    const val RO_SF_LCD_DENSITY = "ro.sf.lcd_density"  // screen density for physical devices
-    const val QEMU_SF_LCD_DENSITY = "qemu.sf.lcd_density"  // screen density for emulated devices
+    const val RO_SF_LCD_DENSITY = "ro.sf.lcd_density" // screen density for physical devices
+    const val QEMU_SF_LCD_DENSITY = "qemu.sf.lcd_density" // screen density for emulated devices
 }
