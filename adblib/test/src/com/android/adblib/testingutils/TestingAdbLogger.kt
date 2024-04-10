400fe78336572e05cc995fce4669de167d0c2530
Renaud Paquay <rpaquay@google.com>
2024-04-10 09:53:11 -0700

    adblib: fix test logger delta instant handling
    * Instead of keeping track of delta instant per logger instance,
    we now centralize this to the (logger) factory so that reading
    entries of a log is easier to comprehend: each delta instant
    value in a log entry is a delta from the previous line instead
    of a delta from the previous line of a given logger instance.
    
    * Improve tracking so that we display delta instants in seconds
    when the number of milliseconds is >= 1000.
    
    All in all, this improve readability of these delta instant
    values, making them more useful when they are needed.
    
    Bug: n/a
    Test: n/a (part of test framework)
    Change-Id: I89817f329345c630b23a3544ed05eff89193a979

--- adblib/test/src/com/android/adblib/testingutils/TestingAdbLogger.kt
+++ adblib/test/src/com/android/adblib/testingutils/TestingAdbLogger.kt
@@ -19,10 +19,21 @@ import com.android.adblib.AdbLogger
 import com.android.adblib.AdbLoggerFactory
 import java.time.Instant
 import java.time.temporal.ChronoUnit
+import java.util.Locale
 
 class TestingAdbLoggerFactory : AdbLoggerFactory {
+    private var previousInstant: Instant? = null
+
+    private val threadNameWidth = 35
 
     var minLevel: AdbLogger.Level = AdbLogger.Level.INFO
+
+    /**
+     * Whether to log with "delta time" from previous log entry. `true` by default, but can be
+     * disabled manually in test code.
+     */
+    @Suppress("MemberVisibilityCanBePrivate")
+    var logDeltaTime: Boolean = true
 
     override val logger: AdbLogger by lazy {
         TestingAdbLogger(this)
@@ -35,23 +46,8 @@ class TestingAdbLoggerFactory : AdbLoggerFactory {
     override fun createLogger(category: String): AdbLogger {
         return TestingAdbLoggerWithPrefix(this, category)
     }
-}
 
-open class TestingAdbLogger(
-    private val factory: TestingAdbLoggerFactory,
-    var logDeltaTime: Boolean = true
-) : AdbLogger() {
-
-    open val prefix: String = "adblib"
-
-    private var previousInstant: Instant? = null
-
-    private val threadNameWidth = 35
-
-    override val minLevel: Level
-        get() = factory.minLevel
-
-    override fun log(level: Level, message: String) {
+    fun logWithPrefix(prefix: String, level: AdbLogger.Level, message: String) {
         if (logDeltaTime) {
             val newInstant = Instant.now()
             synchronized(this) {
@@ -61,7 +57,7 @@ open class TestingAdbLogger(
                     String.format(
                         "[%s%s] [%-${threadNameWidth}s] %7s - %30s - %s",
                         formatInstant(newInstant),
-                        if (logDeltaTime) deltaInstant(newInstant, prevInstant) else "",
+                        deltaInstant(newInstant, prevInstant),
                         Thread.currentThread().name.takeLast(threadNameWidth),
                         level.toString().takeLast(7),
                         prefix.takeLast(30),
@@ -80,14 +76,31 @@ open class TestingAdbLogger(
                 )
             )
         }
+
     }
 
     private fun deltaInstant(newInstant: Instant, prevInstant: Instant): String {
-        // We want to log xxx.y "milliseconds"
-        val micros = ChronoUnit.MICROS.between(prevInstant, newInstant) / 100
-        return if (micros > 0L) {
-            val microsString = String.format("%4s", micros)
-            String.format("(+%s.%sms)", microsString.substring(0, 3), microsString.substring(3, 4))
+        // Depending on the amount of time between newInstant and prevInstance, we want
+        // to log either # of milliseconds or seconds, with one decimal.
+        var unitString: String? = null
+        var unitValue: Long? = null
+
+        val millis = ChronoUnit.MILLIS.between(prevInstant, newInstant)
+        if (millis >= 1000) {
+            // We have at least one second
+            unitString = "s "
+            unitValue = millis
+        } else {
+            val micros = ChronoUnit.MICROS.between(prevInstant, newInstant)
+            if (micros >= 100) {
+                // We have at least one tenth of a milli
+                unitString = "ms"
+                unitValue = micros
+            }
+        }
+        return if (unitValue != null) {
+            assert(unitString != null)
+            String.format(Locale.ROOT, "(+%5.1f%s)", unitValue.toDouble() / 1000, unitString)
         } else {
             "          "
         }
@@ -95,6 +108,20 @@ open class TestingAdbLogger(
 
     private fun formatInstant(newInstant: Instant) =
         newInstant.toString().replace('T', ' ').dropLast(4)
+}
+
+open class TestingAdbLogger(
+    private val factory: TestingAdbLoggerFactory
+) : AdbLogger() {
+
+    open val prefix: String = "adblib"
+
+    override val minLevel: Level
+        get() = factory.minLevel
+
+    override fun log(level: Level, message: String) {
+        factory.logWithPrefix(prefix, level, message)
+    }
 
     override fun log(level: Level, exception: Throwable?, message: String) {
         if (level >= minLevel) {
